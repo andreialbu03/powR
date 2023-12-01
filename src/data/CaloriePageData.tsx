75b127681b2017311cc7402eeb6a6a5c31953a49
Andrei Albu <andrei.albu03@gmail.com>
2023-12-01 16:58:39 -0500

    add data

--- src/data/CaloriePageData.tsx
+++ src/data/CaloriePageData.tsx
@@ -50,4 +50,106 @@ export default [
       },
     ],
   },
+  {
+    id: 2,
+    date: "Tuesday, Oct 17",
+    meals: [
+      {
+        id: 1,
+        mealName: "Breakfast",
+        calories: 550,
+        foods: [
+          {
+            id: 1,
+            name: "Apple",
+          },
+          {
+            id: 2,
+            name: "Yogurt",
+          },
+        ],
+      },
+      {
+        id: 2,
+        mealName: "Lunch",
+        calories: 900,
+        foods: [
+          {
+            id: 3,
+            name: "Salmon",
+          },
+          {
+            id: 4,
+            name: "Quinoa",
+          },
+        ],
+      },
+      {
+        id: 3,
+        mealName: "Dinner",
+        calories: 1000,
+        foods: [
+          {
+            id: 5,
+            name: "Pasta",
+          },
+          {
+            id: 6,
+            name: "Broccoli",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: "Wednesday, Oct 18",
+    meals: [
+      {
+        id: 1,
+        mealName: "Breakfast",
+        calories: 700,
+        foods: [
+          {
+            id: 1,
+            name: "Orange",
+          },
+          {
+            id: 2,
+            name: "Cereal",
+          },
+        ],
+      },
+      {
+        id: 2,
+        mealName: "Lunch",
+        calories: 750,
+        foods: [
+          {
+            id: 3,
+            name: "Turkey Sandwich",
+          },
+          {
+            id: 4,
+            name: "Chips",
+          },
+        ],
+      },
+      {
+        id: 3,
+        mealName: "Dinner",
+        calories: 1100,
+        foods: [
+          {
+            id: 5,
+            name: "Fish",
+          },
+          {
+            id: 6,
+            name: "Sweet Potatoes",
+          },
+        ],
+      },
+    ],
+  },
 ];
--- src/data/HistoryPageData.tsx
+++ src/data/HistoryPageData.tsx
@@ -188,9 +188,136 @@ export default [
     duration: "1h 25m",
     exercises: [
       {
+        id: 1,
+        name: "Deadlift",
+        prSet: "225lb (x8)",
+        sets: [
+          {
+            id: 1,
+            reps: 8,
+            weight: 185,
+          },
+          {
+            id: 2,
+            reps: 8,
+            weight: 205,
+          },
+          {
+            id: 3,
+            reps: 8,
+            weight: 225,
+          },
+        ],
+      },
+      {
         id: 2,
-        name: "Bench Press",
-        prSet: "85lb (x10)",
+        name: "Squats",
+        prSet: "185lb (x10)",
+        sets: [
+          {
+            id: 1,
+            reps: 10,
+            weight: 135,
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: 155,
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: 185,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Pull-ups",
+        prSet: "BW (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: "BW",
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: "BW",
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: "BW",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Leg Curls",
+        prSet: "80lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 60,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 70,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 80,
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Barbell Rows",
+        prSet: "135lb (x8)",
+        sets: [
+          {
+            id: 1,
+            reps: 8,
+            weight: 115,
+          },
+          {
+            id: 2,
+            reps: 8,
+            weight: 125,
+          },
+          {
+            id: 3,
+            reps: 8,
+            weight: 135,
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "Hammer Curls",
+        prSet: "40lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 30,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 35,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 40,
+          },
+        ],
       },
     ],
   },
@@ -200,9 +327,136 @@ export default [
     duration: "1h 00m",
     exercises: [
       {
+        id: 1,
+        name: "Pull-ups",
+        prSet: "BW (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: "BW",
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: "BW",
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: "BW",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Barbell Rows",
+        prSet: "135lb (x8)",
+        sets: [
+          {
+            id: 1,
+            reps: 8,
+            weight: 115,
+          },
+          {
+            id: 2,
+            reps: 8,
+            weight: 125,
+          },
+          {
+            id: 3,
+            reps: 8,
+            weight: 135,
+          },
+        ],
+      },
+      {
         id: 3,
-        name: "Bench Press",
-        prSet: "85lb (x10)",
+        name: "Bicep Curls",
+        prSet: "30lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 20,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 25,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 30,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Tricep Extensions",
+        prSet: "40lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 30,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 35,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 40,
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Leg Press",
+        prSet: "360lb (x10)",
+        sets: [
+          {
+            id: 1,
+            reps: 10,
+            weight: 270,
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: 320,
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: 360,
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "Hammer Curls",
+        prSet: "35lb (x10)",
+        sets: [
+          {
+            id: 1,
+            reps: 10,
+            weight: 25,
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: 30,
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: 35,
+          },
+        ],
       },
     ],
   },
@@ -212,21 +466,136 @@ export default [
     duration: "1h 36m",
     exercises: [
       {
-        id: 3,
-        name: "Bench Press",
-        prSet: "85lb (x10)",
+        id: 1,
+        name: "Leg Press",
+        prSet: "360lb (x10)",
+        sets: [
+          {
+            id: 1,
+            reps: 10,
+            weight: 270,
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: 320,
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: 360,
+          },
+        ],
       },
-    ],
-  },
-  {
-    id: 5,
-    date: "Tuesday, Oct 13",
-    duration: "1h 48m",
-    exercises: [
+      {
+        id: 2,
+        name: "Hammer Curls",
+        prSet: "40lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 30,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 35,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 40,
+          },
+        ],
+      },
       {
         id: 3,
-        name: "Bench Press",
-        prSet: "85lb (x10)",
+        name: "Deadlift",
+        prSet: "225lb (x8)",
+        sets: [
+          {
+            id: 1,
+            reps: 8,
+            weight: 185,
+          },
+          {
+            id: 2,
+            reps: 8,
+            weight: 205,
+          },
+          {
+            id: 3,
+            reps: 8,
+            weight: 225,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Squats",
+        prSet: "185lb (x10)",
+        sets: [
+          {
+            id: 1,
+            reps: 10,
+            weight: 135,
+          },
+          {
+            id: 2,
+            reps: 10,
+            weight: 155,
+          },
+          {
+            id: 3,
+            reps: 10,
+            weight: 185,
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Barbell Rows",
+        prSet: "135lb (x8)",
+        sets: [
+          {
+            id: 1,
+            reps: 8,
+            weight: 115,
+          },
+          {
+            id: 2,
+            reps: 8,
+            weight: 125,
+          },
+          {
+            id: 3,
+            reps: 8,
+            weight: 135,
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "Bicep Curls",
+        prSet: "30lb (x12)",
+        sets: [
+          {
+            id: 1,
+            reps: 12,
+            weight: 20,
+          },
+          {
+            id: 2,
+            reps: 12,
+            weight: 25,
+          },
+          {
+            id: 3,
+            reps: 12,
+            weight: 30,
+          },
+        ],
       },
     ],
   },
