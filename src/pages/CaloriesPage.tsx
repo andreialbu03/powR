bff5b59261796be40ccc57024ef605168ccd779a
Andrei Albu <andrei.albu03@gmail.com>
2023-12-01 16:43:00 -0500

    remove useless code

--- src/pages/CaloriesPage.tsx
+++ src/pages/CaloriesPage.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import CaloriePageTile from "../components/page-tiles/CaloriePageTile";
 import CaloriePageData from "../data/CaloriePageData";
-import { GoPlus } from "react-icons/go";
 
 export default function CaloriesPage() {
   const [newWorkout, setNewWorkout] = React.useState(false);
-
-  function handleNewWorkout() {
-    setNewWorkout(!newWorkout);
-  }
 
   const tiles = CaloriePageData.map((item) => {
     return (
@@ -23,13 +18,5 @@ export default function CaloriesPage() {
     );
   });
 
-  return (
-    <div className="history-page">
-      {tiles}
-      {/* <div className="GoPlus-container" onClick={handleNewWorkout}>
-        <GoPlus size={40} />
-      </div> */}
-      {/* {newWorkout && <NewWorkoutTile onClose={() => setNewWorkout(false)} />} */}
-    </div>
-  );
+  return <div className="history-page">{tiles}</div>;
 }
