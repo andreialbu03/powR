25a6d7e149cf353b0dde3c3822361db75135e3bd
Andrei Albu <andrei.albu03@gmail.com>
2023-12-06 14:44:46 -0500

    add comments

--- src/components/NewWorkoutDropdownOptions.tsx
+++ src/components/NewWorkoutDropdownOptions.tsx
@@ -1,30 +1,37 @@
 import React from "react";
 
+// Define custom NewWorkoutDropdownOptionsProps type
 type NewWorkoutDropdownOptionsProps = {
   onRemove: () => void;
   onEnterSetReps: () => void;
   onClose: () => void;
 };
 
+// Define the NewWorkoutDropdownOptions component
 const NewWorkoutDropdownOptions: React.FC<NewWorkoutDropdownOptionsProps> = ({
   onRemove,
   onEnterSetReps,
   onClose,
 }) => {
+  // Handler for clicking on the overlay
   const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
     // Prevent clicks on the background from triggering onClose
     event.stopPropagation();
   };
 
+  // Render the NewWorkoutDropdownOptions component
   return (
     <div className="new-workout-dropdown-overlay" onClick={onClose}>
       <div
         className="new-workout-dropdown-options"
         onClick={handleOverlayClick}
       >
+        {/* Option to remove exercise */}
         <div style={{ color: "white" }} onClick={onRemove}>
           Remove Exercise
         </div>
+
+        {/* Option to enter weight and reps */}
         <div style={{ color: "white" }} onClick={onEnterSetReps}>
           Enter Weight & Reps
         </div>
@@ -33,4 +40,5 @@ const NewWorkoutDropdownOptions: React.FC<NewWorkoutDropdownOptionsProps> = ({
   );
 };
 
+// Export the NewWorkoutDropdownOptions component
 export default NewWorkoutDropdownOptions;
--- src/components/NewWorkoutOptionsButton.tsx
+++ src/components/NewWorkoutOptionsButton.tsx
@@ -1,13 +1,17 @@
+// Import icons and styling for the component
 import { IoIosOptions } from "react-icons/io";
 import "./NewWorkoutOptionsButton.css";
 
+// Define custom NewWorkoutOptionsBtnProps type
 type NewWorkoutOptionsBtnProps = {
   handleClick: () => void;
 };
 
+// Define the NewWorkoutOptionsButton component
 export default function NewWorkoutOptionsButtons(
   props: NewWorkoutOptionsBtnProps
 ) {
+  // Render the NewWorkoutOptionsButton component
   return (
     <div
       className="close-button options-workout-button"
