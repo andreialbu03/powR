eea2bbccfc14e1769cc32c8d6b8d305636da3038
Andrei Albu <andrei.albu03@gmail.com>
2023-12-06 11:15:48 -0500

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+// Import necessary components and modules from react-router-dom
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Import custom components for the application
 import TopBar from "./components/TopBar";
 import BottomMenuBar from "./components/BottomMenuBar";
 
+// Import pages for different routes
 import HistoryPage from "./pages/HistoryPage";
 import MindsetPage from "./pages/MindsetPage";
 import WorkoutPage from "./pages/WorkoutPage";
@@ -10,28 +13,36 @@ import LearnPage from "./pages/LearnPage";
 import CaloriesPage from "./pages/CaloriesPage";
 import FirstLoadPage from "./pages/FirstLoadPage";
 
+// Import styles for the main App component
 import "./App.css";
 
+// Main component for the application
 function App() {
-  // const [count, setCount] = useState(0);
-
+  // Render the application
   return (
     <Router>
       <div className="container">
+        {/* Top bar for the application */}
         <TopBar />
+
+        {/* Routes for different pages */}
         <Routes>
           <Route path="/history" element={<HistoryPage />} />
           <Route path="/mindset" element={<MindsetPage />} />
           <Route path="/learn" element={<LearnPage />} />
           <Route path="/calories" element={<CaloriesPage />} />
           <Route path="/powR" element={<FirstLoadPage />} />
+
           {/* Default route, e.g., for the home page */}
           <Route path="/" element={<WorkoutPage />} />
         </Routes>
+
+        {/* Bottom menu bar for the application */}
         <BottomMenuBar />
       </div>
     </Router>
   );
 }
 
+// Export the main component
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+// Import the React and ReactDOM modules
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Import  main application components and styling
 import App from "./App.tsx";
 import "./index.css";
 
+// Render the application and main component
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
