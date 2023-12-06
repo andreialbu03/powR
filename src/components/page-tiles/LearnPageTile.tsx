9c93b8c07a35ee8fb5ee83c3c0f7cd27d71caef5
Andrei Albu <andrei.albu03@gmail.com>
2023-12-06 11:51:33 -0500

    add comments

--- src/components/page-tiles/LearnPageTile.tsx
+++ src/components/page-tiles/LearnPageTile.tsx
@@ -1,3 +1,4 @@
+// Define custom types for paragraphType and LearnPageTileProps
 type paragraphType = {
   id: number;
   text: string;
@@ -10,7 +11,9 @@ type LearnPageTileProps = {
   isLast: boolean;
 };
 
+// Define the LearnPageTile component
 export default function LearnPageTile(props: LearnPageTileProps) {
+  // Map over the paragraph prop to create paragraph elements
   const paragraphs = props.paragraph.map((item) => {
     return (
       <p style={{ color: "#929292", textAlign: "left" }} key={item.id}>
@@ -19,10 +22,16 @@ export default function LearnPageTile(props: LearnPageTileProps) {
     );
   });
 
+  // Render the LearnPageTile component
   return (
     <div className={`history-page-tile ${props.isLast ? "last-tile" : ""}`}>
+      {/* Display the title of the LearnPageTile */}
       <h3 className="history-page-tile-date">{props.tileTitle}</h3>
+
+      {/* Display paragraphs */}
       {paragraphs}
+
+      {/* Display "Read More" link */}
       <a
         className="btn"
         href={props.link}
--- src/pages/LearnPage.tsx
+++ src/pages/LearnPage.tsx
@@ -1,7 +1,10 @@
+// Import necessary components from files
 import LearnPageData from "../data/LearnPageData";
 import LearnPageTile from "../components/page-tiles/LearnPageTile";
 
+// Define the LearnPage component
 export default function LearnPage() {
+  // Map over LearnPageData to create LearnPageTile components
   const tiles = LearnPageData.map((item) => {
     return (
       <>
@@ -15,8 +18,10 @@ export default function LearnPage() {
     );
   });
 
+  // Render the LearnPage component
   return (
     <div className="history-page">
+      {/* Display LearnPageTile components */}
       <div className="history-page-tile-feed">{tiles}</div>
     </div>
   );
