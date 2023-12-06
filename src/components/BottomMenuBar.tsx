4e6a133a2c3a9de9abf913d8f35c4fc82a3a74a5
Andrei Albu <andrei.albu03@gmail.com>
2023-12-06 14:32:19 -0500

    add comments for top and bottom bars

--- src/components/BottomMenuBar.tsx
+++ src/components/BottomMenuBar.tsx
@@ -1,29 +1,42 @@
+// Import necessary modules and components
 import { MdHistory, MdOutlineFastfood } from "react-icons/md";
 import { RiMentalHealthFill } from "react-icons/ri";
 import { FaDumbbell, FaBook } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-import "./BottomMenuBar.css"; // Create a CSS file for styling if needed
+// Import styling for the component
+import "./BottomMenuBar.css";
 
+// Define the BottomMenuBar component
 export default function BottomMenuBar() {
+  // Render the BottomMenuBar component
   return (
     <div className="bottom-menu-bar">
+      {/* Navigation link to History section */}
       <Link to="/history" className="bottom-menu-bar-button">
         <MdHistory size={40} />
         <span>History</span>
       </Link>
+
+      {/* Navigation link to Mindset section */}
       <Link to="/mindset" className="bottom-menu-bar-button">
         <RiMentalHealthFill size={40} />
         <span>Mindset</span>
       </Link>
+
+      {/* Navigation link to Workout section */}
       <Link to="/" className="bottom-menu-bar-button">
         <FaDumbbell size={40} />
         <span>Workout</span>
       </Link>
+
+      {/* Navigation link to Learn section */}
       <Link to="/learn" className="bottom-menu-bar-button">
         <FaBook size={40} />
         <span>Learn</span>
       </Link>
+
+      {/* Navigation link to Calories section */}
       <Link to="/calories" className="bottom-menu-bar-button">
         <MdOutlineFastfood size={40} />
         <span>Calories</span>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,27 +1,41 @@
+// Import necessary modules and components from React and other files
 import React from "react";
 import ShareMenu from "./ShareMenu";
 import { IoShareSocial } from "react-icons/io5";
 
+// Import styling for the component
 import "./TopBar.css";
 
-export default function BottomMenuBar() {
+// Define the TopBar component
+export default function TopBar() {
+  // State to track the ShareMenu's open/closed status
   const [isShareOpen, setIsShareOpen] = React.useState(false);
 
+  // Function to handle the Share button click and toggle ShareMenu visibility
   function handleShareClick() {
     setIsShareOpen(!isShareOpen);
   }
 
+  // Render the TopBar component
   return (
     <div className="top-bar">
+      {/* Application title */}
       <h1 className="title">powR</h1>
+
+      {/* Share button container with click handler and color styling */}
       <div
         className="share-container"
         onClick={handleShareClick}
         style={{ color: isShareOpen ? "#3549ff" : "white" }}
       >
+        {/* Share icon */}
         <IoShareSocial size={30} />
+
+        {/* Share text */}
         <span className="share-text">Share</span>
       </div>
+
+      {/* Conditionally render ShareMenu */}
       {isShareOpen && <ShareMenu handleClick={handleShareClick} />}
     </div>
   );
